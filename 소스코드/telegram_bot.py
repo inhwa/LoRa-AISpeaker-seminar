7390f0688ec7424013f4a9fcf10b97495a598a2d
DevHyung <khuphj@gmail.com>
2019-01-26 07:00:24 +0900

    Modify telegram token

--- 소스코드/telegram_bot.py
+++ 소스코드/telegram_bot.py
@@ -1,13 +1,14 @@
 import requests
 import json
-TOKEN = 'INPUT_YOUR_TOKEN' #토큰을 변수에 저장
+TOKEN = '765161150:AAFjbxQFyr_4LTLrOtpQDMSr0lh_Nv0DBN8' #토큰을 변수에 저장
 Bot_URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
 #먼저 봇에게 아무 메세지나 보내세요 ~!
 html = requests.get(Bot_URL+'getUpdates')
-print(Bot_URL+'getUpdates')
+
 jsonStr = json.loads(html.text)
 chat_id = jsonStr['result'][0]['message']['chat']['id']
+print(chat_id)
 chat_url = "https://api.telegram.org/bot{}/sendMessage?chat_id={}".format(TOKEN, chat_id)
 
 while True:
